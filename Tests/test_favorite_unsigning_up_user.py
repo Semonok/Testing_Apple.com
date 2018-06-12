4b745d9db3ee1ec9fe07ea4910a2cc2c8c480c9e
Semonok <semonok95@gmail.com>
2018-06-12 18:06:20 +0500

    oops

--- Tests/test_favorite_unsigning_up_user.py
+++ Tests/test_favorite_unsigning_up_user.py
@@ -1,4 +1,4 @@
 
-def test_favourite_unsigning_up_user(driver):
+def test_favorite_unsigning_up_user(driver):
     driver.header_block.open_favorites_page()
     assert driver.current_page == "LoginPage"
