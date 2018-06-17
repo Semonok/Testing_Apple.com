7d4a3a842a16c39ea5cbaf184ab07975a50d4602
Semonok <semonok95@gmail.com>
2018-06-18 02:10:21 +0500

    created test for checking deleting item in bag

---
+++ Tests/bag_tests/test_del_item_from_bag.py
@@ -0,0 +1,9 @@
+
+def test_add_item_to_bag(driver):
+    driver.header_block.open_mac_page()
+    driver.mac_page.open_buy_page_imac_pro()
+    driver.imac_pro_buy_page.open_customize_page()
+    driver.customize_imac_pro_buy_page.add_to_bag()
+    driver.imac_pro_accessories_page.review_bag()
+    driver.bag_page.remove_item()
+    assert driver.bag_page.main_text == "Your bag is empty."
