e4b559584e80723b724afc99ae9316aa746dc30e
S.Chesnokov <30627443+Semonok@users.noreply.github.com>
2018-06-21 11:40:25 +0500

    refactor method

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
 def converting_price(price, quantity=1):
     s = quantity*float((price.replace(',', "")[1:]))
-    return ','.join(((price.replace(price[1:-1],str(s)))[:-6], (price.replace(price[1:-1],str(s)))[-6:]))
+    if len(str(s)) > 5:
+        return ','.join(((price.replace(price[1:-1],str(s)))[:-6], (price.replace(price[1:-1],str(s)))[-6:]))
+    else:
+        return price.replace(price[1:-1], str(s))
