496043ffce30ae31e92d4f1676ed1f068b261cb3
Semonok <semonok95@gmail.com>
2018-06-25 04:00:58 +0500

    add methods for tests

--- Pages/bag_page.py
+++ Pages/bag_page.py
@@ -2,17 +2,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from Locators.locators import BagPageLocators
 from selenium.webdriver.support.ui import Select
-from model import *
+from model import ConvertingPrice
 from selenium.webdriver.common.keys import Keys
+from data.json_data import JsonActions
+import time
 
 class BagPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 5)
+        self.wait = WebDriverWait(driver, 3)
         self.locators = BagPageLocators
+        self.myjson = JsonActions()
+        self.convert = ConvertingPrice()
         self.item_prices = []
-        self.total_prices = []
+        self.subtotal_prices = []
 
     @property
     def main_text(self):
@@ -34,8 +38,26 @@ class BagPage:
         return self.driver.find_element(*self.locators.item_price).text
 
     @property
+    def subtotal_price(self):
+        return self.driver.find_element(*self.locators.subtotal_price).text
+
+    @property
+    def tax_price(self):
+        price = self.driver.find_element(*self.locators.summary_tax_value).text
+        if price == "$ –":
+            return None
+        else:
+            return price
+
+    @property
     def total_price(self):
         return self.driver.find_element(*self.locators.total_price).text
+
+    def summary_price(self, *price):
+        result_price = 0
+        for i in price:
+            result_price +=self.convert.convert_price_to_float(i)
+        return self.convert.convert_float_to_price(result_price)
 
 
     def item_quantity_less_than_ten(self):
@@ -43,9 +65,10 @@ class BagPage:
         one_item_price = self.item_price
         for i in range(1,10):
             select.select_by_value(str(i))
-            self.wait.until(EC.text_to_be_present_in_element(self.locators.item_price,converting_price(one_item_price,i)))
+            self.wait.until(EC.text_to_be_present_in_element
+                            (self.locators.item_price,self.convert.converting_price(one_item_price,i)))
             self.item_prices.append(self.item_price)
-            self.total_prices.append(self.total_price)
+            self.subtotal_prices.append(self.subtotal_price)
 
     @property
     def current_item_quantity(self):
@@ -75,6 +98,38 @@ class BagPage:
         else:
             return "Select"
 
+    def enter_zip_code(self,zip):
+        jsonfile = self.myjson.read_json_file("zip_codes.json")
+        self.driver.find_element(*self.locators.enter_zip_code).click()
+        self.driver.find_element(*self.locators.zip_code_field).clear()
+        self.driver.find_element(*self.locators.zip_code_field).send_keys(jsonfile[zip])
+        self.driver.find_element(*self.locators.zip_code_apply).click()
+        price = self.driver.find_element(*self.locators.summary_tax_value).text
+        if len(self.driver.find_elements(*self.locators.zip_code_apply)) == 0:
+            self.wait.until_not(EC.text_to_be_present_in_element(self.locators.summary_tax_value, price))
+
+    @property
+    def zip_code_error(self):
+        self.wait.until(EC.presence_of_element_located(self.locators.zip_code_error))
+        return self.driver.find_element(*self.locators.zip_code_error).text
+
+    def cancel_zip_code(self):
+        self.driver.find_element(*self.locators.zip_code_cancel).click()
+
+    def add_applecare_plus(self):
+        self.driver.find_element(*self.locators.applecare_plus_add).click()
+        self.wait.until(EC.presence_of_element_located(self.locators.applecare_plus_remove))
+
+    def remove_applecare_plus(self):
+        self.driver.find_element(*self.locators.applecare_plus_remove).click()
+        self.wait.until(EC.presence_of_element_located(self.locators.applecare_plus_add))
+
+    @property
+    def applecare_plus_price(self):
+        if len(self.driver.find_elements(*self.locators.applecare_plus_price)) == 0:
+            return None
+        else:
+            return self.driver.find_element(*self.locators.applecare_plus_price).text
 
 
 
