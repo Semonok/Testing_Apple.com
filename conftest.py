ff60ed0acd8a20637b65625c48683fc596a60aff
semonok <semonok95@gmail.com>
2018-07-02 22:11:17 +0500

    Changed fixture scope on module

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from Application.application import Application
 
-@pytest.fixture(scope="session")# params=["Chrome", "Firefox", "IE"])
+@pytest.fixture(scope="module")# params=["Chrome", "Firefox", "IE"])
 def driver(request):
     driver = Application("Chrome")
     driver.home_page()
