893e07eff6e9ad1b2793e8fac62797d8f7e8d53b
Semonok <semonok95@gmail.com>
2018-07-15 18:14:36 +0500

    Change browser

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from Application.application import Application
 
 @pytest.fixture(scope="module")# params=["Chrome", "Firefox", "IE"])
 def driver(request):
-    driver = Application("Chrome")
+    driver = Application("Firefox")
     driver.home_page()
     request.addfinalizer(driver.quit)
     return driver
